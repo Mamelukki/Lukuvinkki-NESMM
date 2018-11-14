193ce4abe9bd9ae8e8483daddf70160f5c6b5af8
Mamelukki <salmisar@cs.helsinki.fi>
2018-11-14 18:08:40 +0200

    Created Tip class, Dao interface, TipDao and a database

--- src/lukuvinkki/controller/GreetingController.java
+++ src/lukuvinkki/controller/GreetingController.java
@@ -1,5 +1,6 @@
 package lukuvinkki.controller;
 
+import main.java.lukuvinkki.database.TipDao;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,15 +9,21 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class GreetingController {
 
+    private TipDao tipDao;
+
+    public GreetingController() {
+        this.tipDao = new TipDao("jdbc:sqlite:tip.db");
+    }
+
     @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="title", required=false, defaultValue="Terve") String name, Model model) {
+    public String greeting(@RequestParam(name = "title", required = false, defaultValue = "Terve") String name, Model model) {
         model.addAttribute("title", name);
         return "greeting";
     }
+
     @GetMapping("/bookmark")
-    public String bookmark(@RequestParam(name="title", required=false, defaultValue="Heippa") String name, Model model) {
+    public String bookmark(@RequestParam(name = "title", required = false, defaultValue = "Heippa") String name, Model model) {
         model.addAttribute("title", name);
         return "bookmark";
     }
-
 }
